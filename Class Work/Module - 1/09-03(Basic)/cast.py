61fd45bc9cdd05e900412dad17baae066ec52910
yashahiya <Yashahiya@gmail.com>
2026-03-10 19:53:34 +0530

    commit

---
+++ Class Work/Module - 1/09-03(Basic)/cast.py
@@ -0,0 +1,8 @@
+"""x='54'
+y='76'
+print(int(x)+int(y))"""
+
+
+x=44.22
+print(x)
+print(int(x))
---
+++ Class Work/Module - 1/09-03(Basic)/comment.py
@@ -0,0 +1,4 @@
+#print("Hello Python!")
+
+'''print("Good Afternoon")
+print("Hello Student,This Is Python")'''
