2b5d6becda7bfd9a5beeb829f293ce6772e4f803
yashahiya <Yashahiya@gmail.com>
2026-03-16 15:23:40 +0530

    commit

---
+++ Home Work/Module - 1/19-03/task.py
@@ -0,0 +1,18 @@
+num1=int(input("Enter Number 1 :"))
+num2=int(input("Enter Number 2 : "))
+
+sum=num1+num2
+multi=num1*num2
+
+if num1!=0 and num2!=0 :
+
+    if num1>num2 :
+        print("sum of num1 and num2 is :",sum)
+    else :
+        print("multi of num1 and num2 is : ",multi)
+    
+
+else :
+    print("Please Enter Valid NUmber")
+
+
